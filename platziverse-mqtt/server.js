6987e388dc29b3883911a074dee6d5f1685bbbf4
Ricardo Romo <jricardoromo21@gmail.com>
2018-07-30 23:13:30 -0500

    Agente implementado en el servidor

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,6 +4,9 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
+
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -16,8 +19,96 @@ const settings = {
   backend
 }
 
-const server = new mosca.Server(settings)
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'mrromo',
+  password: process.env.DB_PASS || 'mrromo',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
+}
 
-server.on('ready', () => {
+const server = new mosca.Server(settings)
+const clients = new Map()
+
+let Agent, Metric
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', async (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
+})
+
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
